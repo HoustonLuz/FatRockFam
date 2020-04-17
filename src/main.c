8fbaf577cf8bb53b35db0997468b476a3c3d55da
HoustonLuz <hl16j@my.fsu.edu>
2020-04-17 18:21:05 -0400

    Added control structure & parsing
    makefile & src/makefile
    	-Moved makefile from home directory to inside of src.
    	-Added new rules for running and modified file to do the following:
    		-tar correct parts of repository,
    		-remove correct files in clean case,
    		-and compile correct files.
    
    main.c & main.h
    	-Added basic control structure of program.
    	-Added parsing user instructions.

---
+++ src/main.c
@@ -0,0 +1,89 @@
+#include "main.h"
+
+char	**userInstr;
+int	userArgs;
+
+int main(int argc, const char* argv[]){
+	int	exitFlag = 0,
+		i;
+
+	//Open file
+	FILE* img = fopen(argv[1], "r+");
+	if(img == 0){
+		printf("Incorrect image file.\n");
+		return -1;
+	}
+
+	do {
+		getUserIn();		
+
+		if (userArgs > 0) {
+			//Normal Execution
+			if (strcmp(userInstr[0],"exit") == 0) {
+				exitFlag = 1;
+			} else {
+				//else should not catch any specific instruction.
+				//As such leave this code in so that you know when
+				// an error occurs in catching any instruction.
+				printf("Command WIP.\n");
+			}
+		} else {
+			//Blank instruction, print error.
+			printf("Please enter an instruction.\n");
+		}
+
+	} while (exitFlag == 0);
+
+	//Deallocate user arguments
+	for(i = 0;i < 5;i++)
+		free(userInstr[i]);
+	free(userInstr);
+
+	//printf("Project 3 successfully executed.\n");
+
+	return 0;
+}
+
+void getUserIn(){
+	char	peek;
+	int 	i;
+
+	//Check if userInstr is already allocated.
+	if(userInstr == 0){
+		//Allocate max size.
+		userInstr = (char**) malloc(sizeof(char*) * 5);
+		for (i = 0;i < 5;i++) {
+			userInstr[i] = (char*) malloc(sizeof(char) * 17);
+			userInstr[i][0] = '\0';
+		}
+
+		//printf("Allocated user instruction.\n");
+	}
+
+	//Print prompt.
+	printf("$ ");
+
+	//Read successive strings until 5 arguments are read or
+	// until a new line.
+	i = 0;
+	peek = getc(stdin);
+	while (peek != '\n' && i != 5) {
+		if(peek != ' '){
+			ungetc(peek, stdin);
+			scanf("%16s", userInstr[i]);
+			i++;
+		}
+
+		peek = getc(stdin);
+	}
+
+	userArgs = i;
+
+	/*
+ 	//print parsed command
+	for (i = 0;i < userArgs;i++)
+		printf("%s ", userInstr[i]);
+	if (i != 0)
+		printf("\n");
+	*/
+}
